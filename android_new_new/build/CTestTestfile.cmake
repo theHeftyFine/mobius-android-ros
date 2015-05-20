6b1a88b493637f65a7a80e0a7e9457aba313f399
Student <student@lab-pc.(none)>
2015-05-20 15:58:12 +0200

    More gracefull list attempt -S

---
+++ android_new_new/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/android_new/src
+# Build directory: /home/student/android_new/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(android_core)
